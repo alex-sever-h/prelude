0ccb1f73dcd2ef7e749bb371971d5a60bff269a3
Alexandru-Sever Horin <alexandru.horin@movidius.com>
2017-05-17 17:24:07 +0300

    Change Makefile indent mode

--- personal/60-custom.el
+++ personal/60-custom.el
@@ -36,3 +36,23 @@
 ; This is to override prelude behaviour of C-left C-right
 (custom-set-variables
  '(sp-override-key-bindings (quote (("C-<right>") ("C-<left>")))))
+
+(defun my-makefile-indent-line ()
+  (save-excursion
+(forward-line 0)
+(cond
+ ;; keep TABs
+ ((looking-at "\t")
+  t)
+ ;; indent continuation lines to 4
+ ((and (not (bobp))
+       (= (char-before (1- (point))) ?\\))
+  (delete-horizontal-space)
+  (indent-to 4))
+ ;; delete all other leading whitespace
+ ((looking-at "\\s-+")
+  (replace-match "")))))
+
+(add-hook 'makefile-mode-hook
+      (lambda ()
+    (setq-local indent-line-function 'my-makefile-indent-line)))
